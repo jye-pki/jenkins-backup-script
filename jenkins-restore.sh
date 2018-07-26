65e5f78f49f7eb1ea14118abb0e9b50904122dca
Jill Ye <jill.ye@perkinelmer.com>
2018-07-26 15:07:34 +0800

    add restore script

--- jenkins-restore.sh
+++ jenkins-restore.sh
@@ -1,7 +1,19 @@
 #!/bin/bash -xe
 
+function usage(){
+  echo "usage: $(basename $0) <backup_package.tar.gz> <target_path>\
+ [-n|--nextbuildnumber]\
+ [-p|--needplugin]\
+ [-h|--help]"
+}
+
 readonly BACKUP_PACKAGE=$1
 readonly TARGET_PATH=$2
+
+if [ -z "$BACKUP_PACKAGE" -o -z "$TARGET_PATH" ] ; then
+  usage >&2
+  exit 1
+fi
 
 tar xzvf $BACKUP_PACKAGE -C /tmp
 sudo cp -R /tmp/jenkins-backup/* $TARGET_PATH/
